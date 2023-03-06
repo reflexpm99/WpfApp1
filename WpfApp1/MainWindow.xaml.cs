778ffa3069a742d82b5272d3d3c1de6809437c19
user <user@526-8>
2023-03-06 12:54:18 +0400

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void mi_open_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog s = new();
+            s.ShowDialog();
+            if (string.IsNullOrEmpty(s.FileName))
+                return;
+
+            tb_content.Text = File.ReadAllText(s.FileName);
+        }
+
+        private void mi_save_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            tb_content.Height = this.Height;
+        }
+    }
+}
